4cc1500838a3a76114533dabfcbe7163313f7518
MS2104 <31002@ma-web.nl>
2023-10-19 15:06:56 +0200

    Wave management system set up.

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -5,25 +5,46 @@ using UnityEngine;
 public class WaveManager : MonoBehaviour
 {
     [Header("Wave info")]
-    int waveCount = 1;
-    int waveSize = 1;
-    int enemiesLeft = 1;
+    [SerializeField] private int currentWaveNumber, currentWaveSize, enemiesSpawned, enemiesAlive;
+
+    float spawnInterval = 2f;
 
     public Transform spawnpoint;
     public GameObject[] enemyObj;
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        currentWaveSize += 5;
+        currentWaveNumber = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        spawnEnemy();
+
+        if (enemiesSpawned == currentWaveSize && enemiesAlive == 0)
         {
-            Object.Instantiate(enemyObj[0], spawnpoint.position, Quaternion.identity);
+            currentWaveNumber++;
+        }
+    }
+
+    IEnumerator spawnEnemy()
+    {
+        while (enemiesSpawned <= currentWaveSize)
+        {
+            
+            Instantiate(enemyObj[0], spawnpoint.position, Quaternion.identity);
+            enemiesSpawned++;
+            yield return new WaitForSeconds(spawnInterval);
+
+            if (currentWaveNumber >= 10)
+            {
+                Debug.Log("Wave 10 reached. Upgrading enemies.");
+            }
+
+            yield return new WaitForSeconds(spawnInterval);
         }
     }
 }
