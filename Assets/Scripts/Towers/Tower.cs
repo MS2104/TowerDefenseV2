95af385a275150b05925297290143bd6d43aa8ea
MS2104 <31002@ma-web.nl>
2023-11-02 05:44:12 +0100

    Update Tower.cs

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -7,9 +7,4 @@ public class Tower : MonoBehaviour
 {
     public int level = 1;
     public int damage = 10;
-<<<<<<< Updated upstream
-=======
-
-    public TextMeshProUGUI levelDisplay;
->>>>>>> Stashed changes
 }
